f91c6d936999b900677c54eebdc48617e0e16b28
Victor <vicvarela96@gmail.com>
2019-09-24 10:25:12 -0400

    -Se convierten textview en botones

--- app/src/main/java/com/example/layoutvictorvarela/MainActivity.java
+++ app/src/main/java/com/example/layoutvictorvarela/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.layoutvictorvarela;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,11 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView Text1 = (TextView) findViewById(R.id.Etiqueta);
-        Text1.setText("Text view 1");
-        TextView Text2 = (TextView) findViewById(R.id.Etiqueta2);
-        Text2.setText("Text view 2");
-        TextView Text3 = (TextView) findViewById(R.id.Etiqueta3);
-        Text3.setText("Text view 3");
+
     }
 }
